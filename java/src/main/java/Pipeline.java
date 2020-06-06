77e7ae89d5d85447a1ebe9d25f5893ac0e92e9da
Cédric Tran-Xuan <cedric.tran-xuan@forgerock.com>
2020-06-06 15:02:39 +0200

    refactor(): separated actions from computations and data

--- java/src/main/java/Pipeline.java
+++ java/src/main/java/Pipeline.java
@@ -20,40 +20,55 @@ public class Pipeline {
 
         if (project.hasTests()) {
             if ("success".equals(project.runTests())) {
-                log.info("Tests passed");
                 testsPassed = true;
             } else {
-                log.error("Tests failed");
                 testsPassed = false;
             }
         } else {
-            log.info("No tests");
             testsPassed = true;
+        }
+
+        if (project.hasTests()) {
+            if ("success".equals(project.runTests())) {
+                log.info("Tests passed");
+            } else {
+                log.error("Tests failed");
+            }
+        } else {
+            log.info("No tests");
         }
 
         if (testsPassed) {
             if ("success".equals(project.deploy())) {
-                log.info("Deployment successful");
                 deploySuccessful = true;
             } else {
-                log.error("Deployment failed");
                 deploySuccessful = false;
             }
         } else {
             deploySuccessful = false;
         }
 
+        if (testsPassed) {
+            if ("success".equals(project.deploy())) {
+                log.info("Deployment successful");
+            } else {
+                log.error("Deployment failed");
+            }
+        }
+
         if (config.sendEmailSummary()) {
             log.info("Sending email");
+            String message;
             if (testsPassed) {
                 if (deploySuccessful) {
-                    emailer.send("Deployment completed successfully");
+                    message = "Deployment completed successfully";
                 } else {
-                    emailer.send("Deployment failed");
+                    message = "Deployment failed";
                 }
             } else {
-                emailer.send("Tests failed");
+                message = "Tests failed";
             }
+            emailer.send(message);
         } else {
             log.info("Email disabled");
         }
